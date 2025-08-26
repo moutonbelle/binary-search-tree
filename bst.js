3c68fa8507a2183002ae4472b4148ac20c329faf
Mouton Belle <219940491+moutonbelle@users.noreply.github.com>
2025-08-25 22:06:21 -0500

    Add insert fn to add nodes to binary search tree

--- bst.js
+++ bst.js
@@ -1,30 +1,61 @@
 export default class Tree {
-  constructor(values) {
-    this.root = this.buildTree(values);
-  }
-
-  buildTree(values) {
-    values.sort((a, b) => a - b);
-    console.log(values);
+  constructor(data) {
+    data.sort((a, b) => a - b);
 
     // Deduplicate sorted array
-    for (let i = 0; i < values.length; i++) {
+    for (let i = 0; i < data.length; i++) {
       let j = i + 1;
-      if (j < values.length && values[j] === values[i]) {
+      if (j < data.length && data[j] === data[i]) {
         let duplicates = 1;
-        for (j += 1; j < values.length && values[j] === values[i]; j++)
-          duplicates++;
-        values.splice(i + 1, duplicates);
+        for (j += 1; j < data.length && data[j] === data[i]; j++) duplicates++;
+        data.splice(i + 1, duplicates);
       }
     }
 
-    console.log(values);
+    this.root = this.buildTree(data);
+  }
+
+  buildTree(data) {
+    if (data.length === 0) return null;
+    if (data.length === 1) return new Node(data[0]);
+
+    let mid = Math.floor(data.length / 2);
+    let newNode = new Node(data[mid]);
+    newNode.left = this.buildTree(data.slice(0, mid));
+    newNode.right = this.buildTree(data.slice(mid + 1, data.length));
+    return newNode;
+  }
+
+  insert(data) {
+    if (this.root === null) {
+      this.root = new Node(data);
+      return;
+    }
+
+    let curr = this.root;
+    while (curr !== null) {
+      if (data === curr.data) return;
+      if (data < curr.data) {
+        if (curr.left !== null) curr = curr.left;
+        else {
+          curr.left = new Node(data);
+          return;
+        }
+      }
+      if (data > curr.data) {
+        if (curr.right !== null) curr = curr.right;
+        else {
+          curr.right = new Node(data);
+          return;
+        }
+      }
+    }
   }
 }
 
 class Node {
-  constructor(value = 0) {
-    this.value = value;
+  constructor(data = 0) {
+    this.data = data;
     this.left = null;
     this.right = null;
   }
